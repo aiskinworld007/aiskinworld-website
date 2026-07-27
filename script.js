cf5a25bda14655dae931c57e9cbe4e1e99ee26e3
aiskinworld007 <bgmilifetournament@gmail.com>
2026-07-27 16:08:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const menuButton = document.getElementById('menuButton');
-const mobileMenu = document.getElementById('mobileMenu');
-menuButton.addEventListener('click', () => mobileMenu.classList.toggle('open'));
-mobileMenu.querySelectorAll('a').forEach(link => link.addEventListener('click', () => mobileMenu.classList.remove('open')));
