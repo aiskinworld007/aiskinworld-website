e42252f599dd47abca17376e40123b922007f9db
aiskinworld007 <bgmilifetournament@gmail.com>
2026-07-27 15:54:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const menuButton = document.getElementById('menuButton');
+const mobileMenu = document.getElementById('mobileMenu');
+menuButton.addEventListener('click', () => mobileMenu.classList.toggle('open'));
+mobileMenu.querySelectorAll('a').forEach(link => link.addEventListener('click', () => mobileMenu.classList.remove('open')));
